86659ba5d1f15f403e6e75eb8b79f33e27229807
Олег Луцик <oleglutsik.dev@gmail.com>
2024-06-10 13:25:51 +0300

    Добавьте файлы проекта.

---
+++ WordCounter/Program.cs
@@ -0,0 +1,34 @@
+﻿public static class StringExtensions
+{
+    public static int WordCount(this string str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return 0;
+
+        var words = str.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        return words.Length;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string[] testLines =
+        {
+            "Hello, world!",
+            "This is a test line with several words.",
+            "SingleWord",
+            "",
+            "     ",
+            "Another\ttest\twith\ttabs",
+            "New\nline\ncharacters\nare\nalso\nhandled.",
+            "   Leading and trailing spaces are ignored   "
+        };
+
+        foreach (var line in testLines)
+        {
+            Console.WriteLine($"\"{line}\" has {line.WordCount()} words.");
+        }
+    }
+}
